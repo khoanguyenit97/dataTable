f0218310f27e300b2ab8498eeada214521db0ed8
khoanda10 <khoanda10@gmail.com>
2022-05-10 23:18:12 +0700

    add data

--- app/datatables/user_datatable.rb
+++ app/datatables/user_datatable.rb
@@ -5,7 +5,8 @@ class UserDatatable < AjaxDatatablesRails::ActiveRecord
     # or in aliased_join_table.column_name format
     @view_columns ||= {
       # id: { source: "User.id", cond: :eq },
-      # name: { source: "User.name", cond: :like }
+      name: { source: "User.name" },
+      email: { source: "User.email"}
     }
   end
 
