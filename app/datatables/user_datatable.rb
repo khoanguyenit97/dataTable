ffee73deada018bef77b9ea8b1385115f858985d
khoanda10 <khoanda10@gmail.com>
2022-05-10 22:22:38 +0700

    first commit

---
+++ app/datatables/user_datatable.rb
@@ -0,0 +1,28 @@
+class UserDatatable < AjaxDatatablesRails::ActiveRecord
+
+  def view_columns
+    # Declare strings in this format: ModelName.column_name
+    # or in aliased_join_table.column_name format
+    @view_columns ||= {
+      # id: { source: "User.id", cond: :eq },
+      # name: { source: "User.name", cond: :like }
+    }
+  end
+
+  def data
+    records.map do |user|
+      {
+        # example:
+        id: user.id,
+        name: user.name,
+        email: user.email
+      }
+    end
+  end
+
+  def get_raw_records
+    # insert query here
+    User.all
+  end
+
+end
